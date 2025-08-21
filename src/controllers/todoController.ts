9b8c84280db9596b4d1edaee4848753f0bea3c18
matheuskdr <matheusgabriellkpo@gmail.com>
2025-08-21 18:09:56 -0300

    task list function

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import { Todo } from "../models/Todo";
 
-export const all = async () => {};
+export const all = async (req: Request, res: Response) => {
+  const list = await Todo.findAll();
+  res.json({ list });
+};
 
 export const add = async () => {};
 export const update = async () => {};
